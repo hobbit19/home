e2bf3b95c6930a6ded751665724e3af56c412db9
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-11-29 13:33:06 +0100

    code mirror less text

--- scripts/index.js
+++ scripts/index.js
@@ -8,33 +8,14 @@
   
   const documentation  = `
 ** web-scratch-buffer **
-https://github.com/hugurp/home
 Home of an empty page.
+https://github.com/hugurp/home`
 
-This is a new \`C-t\` like feature, \`C-e\`.
-\`C-e\` opens to a new scratch buffer page
-in which you can write anything.
+  /* This is a new \`C-t\` like feature, \`C-e\`.
+     \`C-e\` opens to a new scratch buffer page
+   * in which you can write anything.*/
 
-# Install 
-- [ ] define how to bind \`C-e\` to new web-scratch-buffer
-
-# Future 
-- [ ] find how to bind the opening new-web-buffer
-- [ ] menu to chose terminal preferences
-- [ ] define preferences, what can be customized
-- [ ] save these preferences localy
-- [ ] save to local memory?
-- [ ] save to Github?
-- [ ] save to Google Drive {.txt,.org.md}?
-- [ ] save to disk?
-- [x] create index.{html.css} to display empty codemirror.
-- [x] insert this message?
-- [ ] check https://github.com/remotestorage, how to store data?
-- [ ] https://github.com/oskarrough/programmingfonts
-- [ ] shared scratch buffer? public? duplicate data?
-`
-
-  const CodeMirrorApp = function(element) {
+			      const CodeMirrorApp = function(element) {
     return CodeMirror(element, {
       value: documentation,
       mode:  "javascript"
